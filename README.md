a459abb5908c4d697fd2d71d8660faa5aa7a2033
Eyewavu <ew.music.yt@gmail.com>
2022-11-08 17:17:48 +0100

    build

--- README.md
+++
@@ -1,6 +0,0 @@
-# React Hangman game
-
-
-
-## images used: https://miroslawzelent.pl/kurs-javascript/przetwarzanie-lancuchow-gra-wisielec/
-## api used: https://github.com/mcnaveen/Random-Words-API
--- src/HangmanImage.tsx
+++
@@ -1,8 +0,0 @@
-export type HangmanImageProps ={ guess:number }
-export default function HangmanImage ({ guess }:HangmanImageProps) {
-  if ( guess > 9 || guess < 0 ) throw new Error("Invalid guess number")
-
-  return (
-    <img src={`./s${guess}.jpg`} alt="" />
-  )
-}
